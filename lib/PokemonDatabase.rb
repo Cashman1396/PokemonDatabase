8f64f0be3397c5b689b5831406e3676e04c2edd9
Chris Ashman <chrisashman96@gmail.com>
2020-02-29 22:05:58 +0000

    update files

--- lib/PokemonDatabase.rb
+++ lib/PokemonDatabase.rb
@@ -1,4 +1,8 @@
-require "PokemonDatabase/version"
+require "open-uri"
+require "nokogiri"
+
+
+require_relative "./PokemonDatabase/version"
 
 module PokemonDatabase
   class Error < StandardError; end
