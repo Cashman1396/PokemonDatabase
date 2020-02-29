86a840ea3566093b4f77c344813d05579de533a5
Chris Ashman <chrisashman96@gmail.com>
2020-02-29 18:01:07 +0000

    first commit

---
+++ lib/PokemonDatabase.rb
@@ -0,0 +1,6 @@
+require "PokemonDatabase/version"
+
+module PokemonDatabase
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/PokemonDatabase/version.rb
@@ -0,0 +1,3 @@
+module PokemonDatabase
+  VERSION = "0.1.0"
+end
