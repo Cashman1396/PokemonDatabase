793ff2d82b5c530f526f3ce66bf4bb2111ae73ca
Chris Ashman <chrisashman96@gmail.com>
2020-03-02 21:52:42 +0000

    update cli file

--- lib/PokemonDatabase/cli.rb
+++ lib/PokemonDatabase/cli.rb
@@ -2,6 +2,15 @@ PokemonData::CLI
 
   def call
     puts "Welcome to the Portable Pokemon Database"
-    PokemonData::Scraper.
-
+    PokemonData::Scraper.all_pokemon
+    national_pokedex_list
+    menu
+    turn_off
 end
+
+  dex national_pokedex_list
+  input = ""
+  puts "Enter '1' to view Pokemon numbers 1 to 150"
+  puts "Enter '2' to view Pokemon numbers 151 to 300"
+  puts "Enter '3' to view Pokemon numbers 301 to 450"
+  puts "Enter"
