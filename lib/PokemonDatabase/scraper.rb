e66f7be1f43b93631e8d4b7770ff4a597218e3e4
Chris Ashman <chrisashman96@gmail.com>
2020-03-04 20:51:43 +0000

    added comments

--- lib/PokemonDatabase/scraper.rb
+++ lib/PokemonDatabase/scraper.rb
@@ -22,10 +22,16 @@ class PokemonDatabase::Scraper
     doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/#{pokemon.name}"))
     pokemon.info = doc.search('p')[0].text
     pokemon.species = doc.search('tr td')[2].text
+    #returns species
     pokemon.height = doc.search('tr td')[3].text
+    #returns height
+
     pokemon.weight = doc.search('tr td')[4].text
+    #returns weight
+
     pokemon.type = doc.search('tr td a')[0].text
     pokemon.type += "/#{doc.search('tr td a')[1].text}" if doc.search('tr td a')[1].text == "Fire" || doc.search('tr td a')[1].text == "Poison" || doc.search('tr td a')[1].text == "Water" || doc.search('tr td a')[1].text == "Grass" || doc.search('tr td a')[1].text == "Rock" || doc.search('tr td a')[1].text == "Fighting" || doc.search('tr td a')[1].text == "Ground" || doc.search('tr td a')[1].text == "Fairy" || doc.search('tr td a')[1].text == "Steel" || doc.search('tr td a')[1].text == "Flying" || doc.search('tr td a')[1].text == "Psychic" || doc.search('tr td a')[1].text == "Dark" || doc.search('tr td a')[1].text == "Ghost" || doc.search('tr td a')[1].text == "Electric" || doc.search('tr td a')[1].text == "Normal" || doc.search('tr td a')[1].text == "Bug" || doc.search('tr td a')[1].text == "Dragon" || doc.search('tr td a')[1].text == "Ice"
+    #all 18 types in the dex
     pokemon
 
   end
